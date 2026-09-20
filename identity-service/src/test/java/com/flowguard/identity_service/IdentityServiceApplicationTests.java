f4e8f890cd41259f091353671f94056a6bcc7d82
Tajudeen Busari <53045507+TajudeenBusari@users.noreply.github.com>
2026-09-20 09:09:25 +0300

    initial set up for the identity-service set up

---
+++ identity-service/src/test/java/com/flowguard/identity_service/IdentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowguard.identity_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
